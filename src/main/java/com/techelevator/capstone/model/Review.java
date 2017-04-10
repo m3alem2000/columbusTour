e03943a586b2dcdd9fcaf0e02f2de6506b33418f
Devin Distelhorst <dmd356@gmail.com>
2017-04-10 15:52:02 -0400

    review and users model

---
+++ src/main/java/com/techelevator/capstone/model/Review.java
@@ -0,0 +1,42 @@
+package com.techelevator.capstone.model;
+
+public class Review {
+
+	private long landmarkId;
+	private long userId;
+	private String review;
+	private long rating;
+
+	public long getLandmarkId() {
+		return landmarkId;
+	}
+
+	public void setLandmarkId(long landmarkId) {
+		this.landmarkId = landmarkId;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+	public long getRating() {
+		return rating;
+	}
+
+	public void setRating(long rating) {
+		this.rating = rating;
+	}
+
+}
