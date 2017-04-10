4d947df21472667e7c9c4a89c1733772b83973bb
Mohammad Ramadan <m3alem@hotmail.com>
2017-04-10 12:38:43 -0400

    work

---
+++ src/main/java/com/techelevator/capstone/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.techelevator.capstone.controller;
+
+import java.util.Map;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+	@Controller
+	public class HomeController {
+		
+		@RequestMapping(path="/", method=RequestMethod.GET)
+		public String showHomePage(Map<String, Object> model) {
+			//model.put("messages", messageDAO.getPublicMessages(10));
+			return "home";
+		}
+}
