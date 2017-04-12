18f299f5100af99014ee83311cde8d3899287b2e
Devin Distelhorst <dmd356@gmail.com>
2017-04-12 10:16:55 -0400

    UserStartingPoint Model

--- src/main/java/com/techelevator/capstone/model/UserStartingPoint.java
+++ src/main/java/com/techelevator/capstone/model/UserStartingPoint.java
@@ -1,5 +1,36 @@
 package com.techelevator.capstone.model;
 
 public class UserStartingPoint {
-
+	private long userId;
+	private String fullAddress;
+	private double latitude;
+	private double longitude;
+	
+	
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getFullAddress() {
+		return fullAddress;
+	}
+	public void setFullAddress(String fullAddress) {
+		this.fullAddress = fullAddress;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	
 }
